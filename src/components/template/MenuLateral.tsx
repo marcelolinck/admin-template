11bec2f1815846eadaa32fc08eac07f6db3baaed
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-08-30 11:41:49 -0400

    Menu em andamento.

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,7 +1,14 @@
-export default function MenuLateral(){
-    return(
+import { IconeAjustes, IconeCasa, IconeSino } from "../Icons"
+import MenuItem from "./MenuItem"
+
+export default function MenuLateral() {
+    return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuItem url="/" texto="Início" icone={IconeCasa} />
+                <MenuItem url="/Ajustes" texto="Ajustes" icone={IconeAjustes} />
+                <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+            </ul>
         </aside>
     )
 }
