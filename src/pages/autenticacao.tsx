0a3072a3a3607dcacc448d0ac61ec2dddf15c64b
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2023-09-05 17:45:38 -0400

    Tela de login em andamento.

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -8,23 +8,51 @@ export default function Autenticacao() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
 
+    function submeter() {
+        if (modo === 'login') {
+            console.log('login')
+        } else {
+            console.log('cadastrar')
+        }
+    }
+
     return (
-        <div>
-            <h1>Autenticacao</h1>
-            <AuthInput
-                label="E-mail"
-                tipo="email"
-                valor={email}
-                valorMudou={setEmail}
-            />
+        <div className="flex h-screen items-center justify-center">
+            <div className="hidden md:block md:w-1/2">
+                <img src="https://source.unsplash.com/random" alt="Imagem da tela de Autenticação" className="h-screen w-full object-cover" />
+            </div>
 
-            <AuthInput
-                label="Senha"
-                valor={senha}
-                tipo="password"
-                valorMudou={setSenha}
-            />
+            <div className="m-10 w-1/2">
+                <h1 className="text-xl font-bold mb-5">
+                    {modo === 'login' ? 'Entre com sua conta' : 'Cadastre-se na Plataforma'}
+                </h1>
+                <AuthInput
+                    label="E-mail"
+                    tipo="email"
+                    valor={email}
+                    valorMudou={setEmail}
+                />
 
+                <AuthInput
+                    label="Senha"
+                    valor={senha}
+                    tipo="password"
+                    valorMudou={setSenha}
+                />
+
+                <button onClick={submeter}
+                    className={`w-full bg-indigo-500 hover:bg-indigo-400
+            text-white rounded-lg px-4 py-3 mt-6`}>
+                    {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+                </button>
+                <hr className="my-6 border-gray-300 w-full" />
+                <button onClick={submeter}
+                    className={`w-full bg-red-500 hover:bg-red-400
+            text-white rounded-lg px-4 py-3 mt-6`}>
+                    Entrar com o Google
+                </button>
+
+            </div>
         </div>
     )
 }
