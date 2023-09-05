2980a699a0b6848698b357546fc5a823abcda0da
marcelolinck <malveslinck@gmail.com>
2023-09-05 06:39:04 -0400

    Aula de AUTH em amdamento.

--- src/components/auth/Authinput.tsx
+++ src/components/auth/Authinput.tsx
@@ -1,8 +1,25 @@
-export default function AuthInput(props){
+
+
+interface AuthInputProps{
+    label: string
+    valor: any
+    obrigatorio?:boolean
+    tipo?: 'text'|'email'|'password'
+    valorMudou: (novoValor: any) => void
+}
+
+export default function AuthInput(props: AuthInputProps){
+    
+
     return(
         <div>
-            <label htmlFor=""></label>
-            <input type="text" />
+            <label>{props.label}</label>
+            <input 
+                type={props.tipo ?? 'text'} 
+                value={props.valor}
+                onChange={e => props.valorMudou?.(e.target.value)}
+                required={props.obrigatorio}
+                />
         </div>
     )
 }
--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,7 +1,30 @@
-export default function Autenticacao(){
-    return(
+import { useState } from "react"
+import AuthInput from "../components/auth/AuthInput"
+
+export default function Autenticacao() {
+
+    //Usando o useState dessa forma, ele irá aceitar somente dois modos conforme abaixo, mas pode ser quantos forem necessarios
+    const [modo, setModo] = useState<'login' | 'cadastro'>('login')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    return (
         <div>
-            Autenticacao
+            <h1>Autenticacao</h1>
+            <AuthInput
+                label="E-mail"
+                tipo="email"
+                valor={email}
+                valorMudou={setEmail}
+            />
+
+            <AuthInput
+                label="Senha"
+                valor={senha}
+                tipo="password"
+                valorMudou={setSenha}
+            />
+
         </div>
     )
 }
